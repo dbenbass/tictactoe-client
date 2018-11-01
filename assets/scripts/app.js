b5af3882f3faed5d874ccbc871d0f872ca9bdf1c
dbenbass <dbenbass@gmail.com>
2018-11-01 10:51:50 -0400

    Commit 11/1 - Add functioning sign in

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,12 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  const authEvents = require('./auth/events.js')
+
+  $(() => {
+    $('#sign-up').on('submit', authEvents.onSignUp)
+    $('#sign-in').on('submit', authEvents.onSignIn)
+    $('#change-password').on('submit', authEvents.onChangePassword)
+    $('#sign-out').on('submit', authEvents.onSignOut)
+  })
 })
